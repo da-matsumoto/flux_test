a057aacc56850b274e7585ce1d0b8bc94be23e1e
da-matsumoto <da.mats1990@gmail.com>
2018-04-20 22:16:41 +0900

    storeとDispatcherにコールバック関数を登録

---
+++ flux_test/src/stores.js
@@ -0,0 +1,19 @@
+import {appDispatcher} from './appDispatcher.js'
+import {ActionType} from './actions.js'
+
+export const nameStore = {name: '', onChange: null}
+export const messageStore = {name: '', onChange: null}
+
+appDispatcher.register(payload => {
+    if (payload.ActionType === ActionType.CHANGE_NAME) {
+        nameStore.name = payload.value
+        nameStore.onChange()
+    }
+})
+
+appDispatcher.register(payload => {
+    if (payload.ActionType === ActionType.SUBMIT_NAME) {
+        messageStore.message = nameStore.name + 'さん、こんにちは。'
+        messageStore.onChange()
+    }
+})
